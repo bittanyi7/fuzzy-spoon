01dae58605a05b02e80fa955fdbbe96b7f1782fa
bittanyi7 <38187009+bittanyi7@users.noreply.github.com>
2018-04-09 22:56:52 +0800

    New test

--- README.md
+++ README.md
@@ -12,7 +12,6 @@ This is a classwork
   1.准备测试以及项目上传，学习使用GitHub
     学习Gitgub完成 共用时2小时
   2.准备编写博客内容
-    博客写了，完事了
 */
   
   
--- shuduku.cpp
+++ shuduku.cpp
@@ -7,22 +7,23 @@ using namespace std;
 
 struct node
 {
-	int col,row;
+	int col, row;
 }zero[81];
 
 int outarr[9][9];
 int shuduku[9][9];
-int A33[6][3] = { {0,1,2},{0,2,1},{1,0,2},{1,2,0},{2,1,0},{2,0,1} };
+int A33[6][3] = { { 0,1,2 },{ 0,2,1 },{ 1,0,2 },{ 1,2,0 },{ 2,1,0 },{ 2,0,1 } };
 int seedshuduku[9][9] =
-{ { 3,9,4,5,7,1,6,2,8 },
-{ 2,6,8,4,3,9,7,5,1 },
-{ 7,5,1,8,6,2,3,9,4 },
-{ 5,8,3,1,2,6,9,4,7 },
-{ 9,4,7,3,5,8,1,6,2 },
-{ 6,1,2,7,9,4,8,3,5 },
-{ 1,3,5,9,4,7,2,8,6 },
-{ 4,7,6,2,8,3,5,1,9 },
-{ 8,2,9,6,1,5,4,7,3 }
+{ 
+{ '3','9','4','5','7','1','6','2','8' },
+{ '2','6','8','4','3','9','7','5','1' },
+{ '7','5','1','8','6','2','3','9','4' },
+{ '5','8','3','1','2','6','9','4','7' },
+{ '9','4','7','3','5','8','1','6','2' },
+{ '6','1','2','7','9','4','8','3','5' },
+{ '1','3','5','9','4','7','2','8','6' },
+{ '4','7','6','2','8','3','5','1','9' },
+{ '8','2','9','6','1','5','4','7','3' },
 };
 
 FILE *a;
@@ -30,10 +31,10 @@ FILE *b;
 int cout = 0;
 int flag = 0;
 int n = 0;
-int shudukuIsOk=0;
-int boder=0;
+int shudukuIsOk = 0;
+int boder = 0;
 
-void Exechage(int &a,int &b)
+void Exechage(int &a, int &b)
 {
 	int temp;
 	temp = a;
@@ -43,25 +44,27 @@ void Exechage(int &a,int &b)
 
 void OutBreak()
 {
-//	fprintf(a, "%d\n", cout+1);
+	//	fprintf(a, "%d\n", cout+1);
 	for (int i = 0; i < 9; i++)
 	{
 		for (int j = 0; j < 9; j++)
 		{
 			if (j <8)
 			{
-				fprintf(a, "%d ", outarr[i][j]);
+				fputc(outarr[i][j] , a);
+				fputc(' ', a);
 			}
 			else
 			{
-				fprintf(a, "%d\n", outarr[i][j]);
+				fputc(outarr[i][j], a);
+				fputc('\n', a);
 			}
 		}
 	}
 	cout++;
-	if(cout<n)
+	if (cout<n)
 	{
-		fprintf(a, "\n");
+		fputc('\n', a);
 	}
 	else
 	{
@@ -69,78 +72,78 @@ void OutBreak()
 	}
 }
 
-void Ex(int a, int b,int c,int cur)
+void Ex(int a, int b, int c, int cur)
 {
 	for (int i = 0; i < 9; i++)
 	{
-		outarr[cur][i] = seedshuduku[a+cur][i];
-		outarr[cur+1][i] = seedshuduku[b+cur][i];
-		outarr[cur+2][i] = seedshuduku[c+cur][i];
+		outarr[cur][i] = seedshuduku[a + cur][i];
+		outarr[cur + 1][i] = seedshuduku[b + cur][i];
+		outarr[cur + 2][i] = seedshuduku[c + cur][i];
 	}
 }
-int juge(int col,int row)
+int juge(int col, int row)
 {
 	int temp[10];
-	int row9=row/3*3;
-	int col9=col/3*3;
-	memset(temp,0,sizeof(temp));
-	for(int i=0;i<9;i++)
+	int row9 = row / 3 * 3;
+	int col9 = col / 3 * 3;
+	memset(temp, 0, sizeof(temp));
+	for (int i = 0; i<9; i++)
 	{
 		temp[shuduku[col][i]]++;
 	}
-	for(int i=0;i<10;i++)
+	for (int i = 0; i<10; i++)
 	{
-		if(temp[i]>1)
+		if (temp[i]>1)
 			return 0;
 	}
-	memset(temp,0,sizeof(temp));
-	for(int i=0;i<9;i++)
+	memset(temp, 0, sizeof(temp));
+	for (int i = 0; i<9; i++)
 	{
 		temp[shuduku[i][row]]++;
 	}
-	for(int i=0;i<10;i++)
+	for (int i = 0; i<10; i++)
 	{
-		if(temp[i]>1)
+		if (temp[i]>1)
 			return 0;
 	}
-	memset(temp,0,sizeof(temp));
-	for(int i=0;i<3;i++)
+	memset(temp, 0, sizeof(temp));
+	for (int i = 0; i<3; i++)
 	{
-		for(int j=0;j<3;j++)
+		for (int j = 0; j<3; j++)
 		{
-			temp[shuduku[i+col9][j+row9]]++;
+			temp[shuduku[i + col9][j + row9]]++;
 		}
 	}
-	for(int i=0;i<10;i++)
+	for (int i = 0; i<10; i++)
 	{
-		if(temp[i]>1)
+		if (temp[i]>1)
 			return 0;
 	}
 	return 1;
 }
 void Soluve(int x)
 {
-	if(x>=boder)
+	if (x >= boder)
 	{
-		shudukuIsOk=1;
-		return ;	
+		shudukuIsOk = 1;
+		return;
 	}
-	for(int i=1;i<=9&&!shudukuIsOk;i++)
+	for (int i = 1; i <= 9 && !shudukuIsOk; i++)
 	{
-		shuduku[zero[x].col][zero[x].row]=i;
-		if(juge(zero[x].col,zero[x].row))
+		shuduku[zero[x].col][zero[x].row] = i;
+		if (juge(zero[x].col, zero[x].row))
 		{
-			Soluve(x+1);
+			Soluve(x + 1);
 		}
 	}
 }
 int main(int argc,char* argv[])
 {
-	if(strcmp(argv[1],"-c")==0)
+	if (strcmp(argv[1], "-c") == 0)
 	{
 		int len = strlen(argv[2]);
 		a = fopen("shuduku.txt", "w");
-		for (int i = 0; i < len; i++) 
+		for (int i = 0; i < len; i++)
 		{
 			if (argv[2][i] >= '0'&&argv[2][i] <= '9')
 				continue;
@@ -155,9 +158,9 @@ int main(int argc,char* argv[])
 			n *= 10;
 			n += argv[2][i] - '0';
 		}
-		int seedarr[8] = { 1,2,4,5,6,7,8,9,};//Fuck!!!
-		int startads = 7,curads=0,changeads;
-		while (startads != 0&&!flag)
+		int seedarr[8] = { '1','2','4','5','6','7','8','9', };//Fuck!!!
+		int startads = 7, curads = 0, changeads;
+		while (startads != 0 && !flag)
 		{
 			curads = startads;
 			startads--;
@@ -172,15 +175,15 @@ int main(int argc,char* argv[])
 					Exechage(seedarr[i], seedarr[j]);
 				}
 				startads = 7;
-				for (int i = 0; i < 9; i++)//??????
+				for (int i = 0; i < 9; i++)
 				{
 					for (int j = 0; j < 9; j++)
 					{
-						int temp = seedshuduku[i][j];
+						int temp = seedshuduku[i][j]-'0';
 						if (temp == 3)
 						{
-							seedshuduku[i][j] = 3;
-							outarr[i][j] = 3;
+							seedshuduku[i][j] = '3';
+							outarr[i][j] = '3';
 							continue;
 						}
 						else if (temp>3)
@@ -195,49 +198,49 @@ int main(int argc,char* argv[])
 						}
 					}
 				}
-				for (int i = 0; i < 6&&!flag; i++)
+				for (int i = 0; i < 6 && !flag; i++)
 				{
-					Ex(A33[i][0], A33[i][1], A33[i][2],3);
+					Ex(A33[i][0], A33[i][1], A33[i][2], 3);
 					for (int j = 0; j < 6 && !flag; j++)
 					{
-						Ex(A33[j][0], A33[j][1], A33[j][2],6);
+						Ex(A33[j][0], A33[j][1], A33[j][2], 6);
 						OutBreak();
 					}
 				}
 			}
-			if (startads == 0&&!flag)
+			if (startads == 0 && !flag)
 				break;
 		}
 		fclose(a);
 		return 0;
 	}
-	else if(strcmp(argv[1],"-s")==0)
+	else if (strcmp(argv[1], "-s") == 0)
 	{
-		a = fopen("shuduku.txt","w");
+		a = fopen("shuduku.txt", "w");
 		b = fopen(argv[2], "r");
 		int temp;
-		int num=0,t=0;
-		while(~fscanf(b,"%d",&temp))
+		int num = 0, t = 0;
+		while (~fscanf(b, "%d", &temp))
 		{
-			int x=num/9;
-			int y=num%9;
+			int x = num / 9;
+			int y = num % 9;
 			num++;
-			shuduku[x][y]=temp;
-			if(temp==0)
+			shuduku[x][y] = temp;
+			if (temp == 0)
 			{
-				zero[boder].col=x;
-				zero[boder].row=y;
+				zero[boder].col = x;
+				zero[boder].row = y;
 				boder++;
 			}
-			if(num==81)
+			if (num == 81)
 			{
-				num=0;
+				num = 0;
 				Soluve(0);
-				boder=0;
-				shudukuIsOk=0;
-				if(t!=0)
+				boder = 0;
+				shudukuIsOk = 0;
+				if (t != 0)
 				{
-					fprintf(a,"\n");
+					fprintf(a, "\n");
 				}
 				t++;
 				for (int i = 0; i < 9; i++)
